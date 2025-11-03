ea05b7256cb220301366334ef6ca7af57c6235dc
ahmadaloraij <147504334+ahmadaloraij@users.noreply.github.com>
2025-11-03 14:54:07 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,10 @@
--- ADD YOUR SQL QUERY HERE
+Select
+it.investor_id
+s.sector_name,
+Round(100.0*it.no_of_shares/ SUM(it.no_of_shares) OVER (PARITTION BY it.investor_id
+), AS share_percentage
+From INVESTOR_TRANSACTION IT
+Join sectors s
+ON it.sector_id=s.sector_id
+Order BY it.investor_id,share percentage DESC;
+python main.py
